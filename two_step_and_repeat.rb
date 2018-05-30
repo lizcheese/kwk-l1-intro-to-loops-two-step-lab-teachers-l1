d5bd1bb0865107480d620b69b6b229b2706cee94
Elizabeth Chesebrough <e.chesebrough@gmail.com>
2018-05-30 23:12:20 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -26,7 +26,7 @@ end
 
 def how_many_steps?
   # Write a loop that fulfills all criteria from Part 3 of the README.md
-  steps = 0
+  steps = 6
     if steps%2 == 0 then puts "Left"
     elsif puts "Right"
     end
